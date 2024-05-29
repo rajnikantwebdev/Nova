36f0e6dcb707f890f3b6ac0ddb6f1792cd85b8df
Rajnikantbhay <rsusishere@gmail.com>
2024-05-29 16:59:19 +0530

    fixing build bugs

--- app/pages/ContactUsPage.jsx
+++ app/pages/ContactUsPage.jsx
@@ -15,7 +15,7 @@ const main = (
           required
         />
         <label
-          for="floating_email"
+          htmlFor="floating_email"
           className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
         >
           Full name
@@ -31,7 +31,7 @@ const main = (
           required
         />
         <label
-          for="floating_password"
+          htmlFor="floating_password"
           className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-white  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
         >
           Password
